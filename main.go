3d986b41703f34aa2543938c5d04eb82f76ecda3
Memory Clutter <memclutter@gmail.com>
2017-06-17 10:11:14 +0300

    File serving

--- main.go
+++ main.go
@@ -6,11 +6,13 @@ import (
 	"log"
 )
 
+var dist = flag.String("dist", "/var/www/html", "Angular frontend dist files")
 var addr = flag.String("addr", ":8080", "Http address and port")
 //var socket = flag.String("socket", "/var/run/docker.sock", "Docker unix-socket file")
 
 func main() {
 	flag.Parse()
+	http.FileServer(http.Dir(dist))
 	http.Handle("/api/", http.HandlerFunc(api))
 	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
