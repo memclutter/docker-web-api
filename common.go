d2cdf5aca12fe77c801cf5244a98e387ac3a2829
Memory Clutter <memclutter@gmail.com>
2017-06-17 15:11:48 +0300

    Containers list and remove HTTP methods

---
+++ common.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"net/http"
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type HttpErrorResponse struct {
+	Message string	`json:"message"`
+}
+
+// Send http error as JSON object like as {"message": "Error occurred"} or raw string
+func SendHttpError(code int, message string, w http.ResponseWriter) {
+	w.WriteHeader(code)
+
+	if body, err := json.Marshal(HttpErrorResponse{Message: message}); err != nil {
+		w.Write([]byte(message))
+	} else {
+		w.Header().Add("Content-Type", "application/json")
+		w.Write(body)
+	}
+
+	log.Printf("HTTP %v: %v", code, message)
+}
+
+// Get query string param, if not exists return defaultValue
+func QueryGetOrDefaultValue(r *http.Request, key string, defaultValue string) string {
+	if value := r.URL.Query().Get(key); value == "" {
+		return defaultValue
+	} else {
+		return value
+	}
+}
+
+// Send JSON data or send error
+func SendJSONOrError(w http.ResponseWriter, data interface{}) {
+	if body, err := json.Marshal(data); err != nil {
+		SendHttpError(500, fmt.Sprintf("%v", err), w)
+	} else {
+		w.WriteHeader(200)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(body)
+	}
+}
+
+// Send empty body
+func SendOk(w http.ResponseWriter) {
+	w.WriteHeader(200)
+	w.Header().Set("Content-Type", "application/json")
+}
--- containers.go
+++ containers.go
@@ -2,30 +2,63 @@ package main
 
 import (
 	"net/http"
-	"log"
 	"context"
-	"strings"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
+	"fmt"
+	"strconv"
 )
 
 func apiContainers(w http.ResponseWriter, r *http.Request, params map[string]string) {
 	docker, err := client.NewEnvClient()
 	if err != nil {
-		log.Panicf("Docker client: %v", err)
+		SendHttpError(500, fmt.Sprintf("Docker: %v", err), w)
+		return
 	}
 
-	containers, err := docker.ContainerList(context.Background(), types.ContainerListOptions{})
+	options := types.ContainerListOptions{}
+	if all, err := strconv.ParseBool(QueryGetOrDefaultValue(r, "all", "false")); err != nil {
+		SendHttpError(400, fmt.Sprintf("Invalid all parameter type %v", err), w)
+		return
+	} else {
+		options.All = all
+	}
+
+	containers, err := docker.ContainerList(context.Background(), options)
 	if err != nil {
-		log.Panicf("Docker client: %v", err)
+		SendHttpError(500, fmt.Sprintf("%v", err), w)
+		return
 	}
 
-	for _, container := range containers {
-		log.Printf("Container: %v", strings.Join(container.Names, ","))
-	}
+	SendJSONOrError(w, containers)
 }
 
 func apiContainersDelete(w http.ResponseWriter, r *http.Request, params map[string]string) {
-	log.Printf("Access to containers remove")
+	docker, err := client.NewEnvClient()
+	if err != nil {
+		SendHttpError(500, fmt.Sprintf("Docker: %v", err), w)
+		return
+	}
+
+	options := types.ContainerRemoveOptions{}
+	if force, err := strconv.ParseBool(QueryGetOrDefaultValue(r, "force", "false")); err != nil {
+		SendHttpError(400, fmt.Sprintf("Invalid force parameter type %v", err), w)
+		return
+	} else {
+		options.Force = force
+	}
+
+	containerID, ok := params["id"]
+	if !ok {
+		SendHttpError(400, "Missing container ID", w)
+		return
+	}
+
+	if err := docker.ContainerRemove(context.Background(), containerID, options); err != nil {
+		SendHttpError(500, fmt.Sprintf("%v", err), w)
+		return
+	}
+
+	SendOk(w)
 }
